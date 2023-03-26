951363a7ad69b0fdd2aa9cebed51c9f3d4397853
Yadav Ranganathan <yadav.ranganathan@gmail.com>
2023-03-27 00:22:19 +0530

    dropdown

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,25 +1,15 @@
 import React from "react";
-import plant from "../assets/plant.png";
+
 function Dropdown() {
-    const dropdown = document.createElement("select");
-
-    const option1 = document.createElement("option");
-    option1.text = "Option 1";
-    dropdown.add(option1);
-
-    const option2 = document.createElement("option");
-    option2.text = "Option 2";
-    dropdown.add(option2);
-
-    const option3 = document.createElement("option");
-    option3.text = "Option 3";
-    dropdown.add(option3);
-
-    dropdown.style.position = "absolute";
-    dropdown.style.left = "50%";
-    dropdown.style.top = "50%";
-    dropdown.style.transform = "translate(-50%, -50%)";
-
+      return (
+        <div className="overflow-hidden flex justify-center items-center h-screen">
+          <select className="block w-64 py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500">
+            <option>Plant 1</option>
+            <option>Plant 2</option>
+            <option>Plant 3</option>
+          </select>
+        </div>
+      );
 }
 
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,7 @@ import plant from "../assets/plant.png";
 function Navbar() {
   return (
     <div>
-      <nav class="flex items-center justify-between flex-wrap bg-teal-700 p-6">
+      <nav class="overflow-hidden flex items-center justify-between flex-wrap bg-teal-700 p-6">
         <div class="flex items-center flex-shrink-0 text-white mr-6">
           <span class="font-bold text-2xl flex flex-row items-center">
             <img className="h-10 w-10 mx-2" src={plant} />
