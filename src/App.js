4a68604e3dd741842429e63f0f3f1e70e4b12a70
manuvikash <manuvikashs@gmail.com>
2023-03-26 22:38:56 +0530

    Added navbar and bg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="h-screen w-screen -z-20"
+      style={{ backgroundImage: "url(/img/bg.jpg)" }}
+    >
+      <Navbar />
+      <div classname="flex text-white text-3xl items-center text-center">
+        <p className=" text-white text-3xl items-center text-center">Hello</p>
+      </div>
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import plant from "../assets/plant.png";
+function Navbar() {
+  return (
+    <div>
+      <nav class="flex items-center justify-between flex-wrap bg-teal-700 p-6">
+        <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-bold text-2xl flex flex-row items-center">
+            <img className="h-10 w-10 mx-2" src={plant} />
+            Gardener
+          </span>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
